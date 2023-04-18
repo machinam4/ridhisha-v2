256cfbce58699c60bf1709b168429174812f46d7
machinawindowscyber <machina@windows.cyber>
2023-04-18 22:06:10 +0300

    paybill accepts multiple radios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description"
-        content="RidhishaJamii Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+    <meta name="description" content="RidhishaJamii, by evoton.co.ke, is the ultimate Mpesa Dashboard." />
     <meta name="keywords"
-        content="admin templates, bootstrap admin templates, bootstrap 4, dashboard, dashboard templets, sass admin templets, html admin templates, responsive, bootstrap admin templates free download,premium bootstrap admin templates, RidhishaJamii, RidhishaJamii bootstrap admin template">
-    <meta name="author" content="Phoenixcoded" />
+        content="RidhishaJamii, Ridhisha, Evoton, evoton.co.ke, evoton systems, evoton softwares, admin templates,">
+    <meta name="author" content="evoton.co.ke" />
 
     <!-- Favicon icon -->
     <link rel="icon" href="{{ asset('assets/images/Ridhishajamii-icon-logo.svg') }}" type="image/x-icon">
